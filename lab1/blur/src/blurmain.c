eb89f0d227a0d32ee57bcdf3ad62417778d55694
Alexander Yngve <aleyn573@student.liu.se>
2016-04-29 01:45:52 +0200

    Some progress.

--- lab1/blur/src/blurmain.c
+++ lab1/blur/src/blurmain.c
@@ -44,11 +44,27 @@ int main (int argc, char ** argv) {
   MPI_Bcast((void*)&ysize, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
   MPI_Bcast((void*)&radius, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
 
-  /* TODO: Allocate memory for own slice of the image */
-  int ystart = get_ystart(ysize, rank, world_size);
-  int yend = get_yend(ysize, rank, world_size);
-  int size = (yend - ystart) * xsize * sizeof(pixel);
-  pixel* data = (pixel*)malloc(size);
+  /* Allocate and send/recieve memory for own slice of the image */
+  int sendcounts[world_size];
+  int displs[world_size];
+  int ystarts[world_size];
+  int yends[world_size];
+
+  int i;
+  for(i = 0; i < world_size; i++){
+    ystarts[i] = get_ystart(ysize, i, world_size);
+    yends[i] = get_yend(ysize, i, world_size);
+    int size = (yends[i] - ystarts[i]) * xsize;
+    sendcounts[i] = size;
+    displs[i] = ystarts[i]*xsize;
+  }
+
+  printf("rank: %d\t ystart: %d\t yend: %d\t diff: %d\t count %d\t displ %d\n", rank, ystarts[rank], yends[rank], yends[rank] - ystarts[rank], sendcounts[rank], displs[rank]);
+
+  pixel* data = (pixel*)malloc(sendcounts[rank] * sizeof(pixel));
+  MPI_Scatterv(&src, sendcounts, displs, mpi_pixel_type, data, sendcounts[rank], mpi_pixel_type, ROOT, MPI_COMM_WORLD);
+
+  /* TODO: Send shit to other processes */
 
   /* Gauss */
   double w[MAX_RAD];
@@ -58,17 +74,20 @@ int main (int argc, char ** argv) {
   clock_gettime(CLOCK_REALTIME, &stime);
 
   /* Filter */
-  blurfilter(xsize, ysize, src, radius, w);
+  blurfilter(xsize, yends[rank]-ystarts[rank], data, radius, w);
 
   clock_gettime(CLOCK_REALTIME, &etime);
 
   printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
 	 1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
 
+
+  MPI_Gatherv(data, sendcounts[rank], mpi_pixel_type, &src, sendcounts, displs, mpi_pixel_type, ROOT, MPI_COMM_WORLD);
+
   /* write result */
-  printf("Writing output file\n");
-  if(rank == ROOT && write_ppm (argv[3], xsize, ysize, (char *)src) != 0)
-    exit(1);
+  if(rank == ROOT) printf("Writing output file\n");
+  if(rank == ROOT && write_ppm (argv[3], xsize, ysize, (char *)src) != 0) exit(1);
+  
 
   /* Exit */
   MPI_Finalize();
