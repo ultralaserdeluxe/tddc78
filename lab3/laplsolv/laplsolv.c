52b27e6e8065a38150922fed448fb300def0378e
Pål Kastman <palka285@student.liu.se>
2016-05-18 16:06:10 +0200

    Removed hardcoded shit.

--- lab3/laplsolv/laplsolv.c
+++ lab3/laplsolv/laplsolv.c
@@ -5,24 +5,8 @@
 #include <math.h>
 #include <omp.h>
 
-#ifdef __MACH__
-#include <sys/time.h>
-
-#define CLOCK_REALTIME 1234
-
-int clock_gettime(int clk_id, struct timespec* t) {
-    struct timeval now;
-    int rv = gettimeofday(&now, NULL);
-    if (rv) return rv;
-    t->tv_sec  = now.tv_sec;
-    t->tv_nsec = now.tv_usec * 1000;
-    return 0;
-}
-
-#endif /* __MACH__ */
 
 #define MAX(A,B) (A > B ? A : B)
-
 #define N 1000
 #define MAX_ITER 1000
 
@@ -50,6 +34,11 @@ void init_T(double* T){
   }
 }
 
+int get_yend(int ysize, int rank, int world_size){
+  int yend = (rank + 1) * ceil((double)ysize / world_size);
+  return ysize < yend ? ysize : yend;
+}
+
 int main() {
   double* T = (double*)malloc((N+2)*(N+2)*sizeof(double));
   init_T(T);
@@ -62,35 +51,34 @@ int main() {
 
   double starttime = omp_get_wtime();
 
-# pragma omp parallel				 \
-  firstprivate(iterations, max_error, new_error) \
-  shared(T, error, tol, stop)	 \
-  num_threads(1)
+# pragma omp parallel					\
+  firstprivate(iterations, max_error, new_error)	\
+  shared(T, error, tol, stop)				\
+  num_threads(7)
   {
     double* tmp1 = (double*)malloc((N+2)*sizeof(double));
     double* tmp2 = (double*)malloc((N+2)*sizeof(double));
     double* tmp3 = (double*)malloc((N+2)*sizeof(double));
 
+    int yend = get_yend(N, omp_get_thread_num(), omp_get_num_threads());
+    int ystart = get_yend(N, omp_get_thread_num()-1, omp_get_num_threads()) + 1;
+
     while(!stop){
       if(omp_get_thread_num() == 0){
-	memcpy(tmp1, T, (N+2)*sizeof(double));
-      }else{
-	memcpy(tmp1, T + (N+2)*500, (N+2)*sizeof(double));
+       	memcpy(tmp1, T, (N+2)*sizeof(double));
+      }else{	
+	memcpy(tmp1, T + (N+2)*yend, (N+2)*sizeof(double));
       }
 
-
-      memcpy(tmp3, T + (N+2)*(500+1), (N+2)*sizeof(double));
-
-/* #     pragma omp single */
-/*       {  for(int i = 0; i < N+2; printf("%f ", tmp3[i++])); printf("\n"); } */
+      int yend = get_yend(N, omp_get_thread_num(), omp_get_num_threads());
+      memcpy(tmp3, T + (N+2)*(yend+1), (N+2)*sizeof(double));
 
 #     pragma omp barrier
-#     pragma omp for schedule(static, 500)
+#     pragma omp for schedule(static, (int)ceil((double)N/omp_get_num_threads()))
       for(int y = 1; y <= N; y++) {
 	memcpy(tmp2, T + (N+2)*y, (N+2)*sizeof(double));
-	    /* printf("thread: %d y: %d iter: %d\n", omp_get_thread_num(), y, iterations); */
 	for(int x = 1; x <= N; x++){
-	  if(y == 500){
+	  if(y == yend){
 	    T[(N+2)*y + x] = (tmp1[x] + tmp2[x-1] + tmp2[x+1] + tmp3[x])/4.0;
 	  }else{
 	    T[(N+2)*y + x] = (tmp1[x] + tmp2[x-1] + tmp2[x+1] + T[(N+2)*(y+1) + x])/4.0;
@@ -98,7 +86,6 @@ int main() {
 	  new_error = fabs(tmp2[x] - T[ + (N+2)*y + x]);
 	  max_error = MAX(new_error, max_error);
 	}
-
 
 	memcpy(tmp1, tmp2, (N+2)*sizeof(double));
       }
@@ -113,8 +100,6 @@ int main() {
 
 #     pragma omp single
       {
-	/* print_T(T); */
-	/* printf("\n"); */
 	if(error < tol || iterations >= MAX_ITER){
 	  stop = 1;
 	}else{
